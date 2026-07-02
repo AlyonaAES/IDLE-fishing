948f4630ed04378d685e929afef4abd64f6fa8c3
AlyonaAES <alyonashtraizent@gmail.com>
2026-07-02 11:45:23 -0400

    nothing

---
+++ Assets/FTM.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FTM : MonoBehaviour
+{
+    
+    public int panelIndex;
+
+    public List<Button> buttons;
+    public int buttonIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
